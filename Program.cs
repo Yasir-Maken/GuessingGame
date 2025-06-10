fa7932b131636c8b79e2975de09c7e489c018d1d
Yasir-Maken <yasir.maken.cs24@gmail.com>
2025-06-10 05:42:10 +0300

    Guessing Word Game: Trying to practice C#

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+
+string secretWord = "yasir";
+string guess = "";
+int guessConunt = 0;
+int guessLimit = 3;
+bool outOfGuesses = false;
+
+while (guess != secretWord && !outOfGuesses)
+{
+    if (guessConunt < guessLimit)
+    {
+        Console.Write("Enter Guess: ");
+        guess = Console.ReadLine();
+        guessConunt++;
+
+    }
+    else
+    {
+        outOfGuesses = true;
+    }
+
+}
+
+
+if (outOfGuesses)
+{
+    Console.WriteLine("You Lose!");
+}
+else
+{
+    Console.WriteLine("You Win!");
+}
+
+
+Console.ReadKey();
